248928dbab58487933ee52b5d9901457c2e9cae9
MeKareem3 <120672116+MeKareem3@users.noreply.github.com>
2023-02-09 21:04:19 -0500

    created

---
+++ leap year checker/main.py
@@ -0,0 +1,18 @@
+# Dont change the code below
+year = int(input("Which year do you want to check "))
+# Dont change the code above
+
+#write your code below
+if (year%4 ==0):
+    print("Leap year")
+elif(year%100 ==0):
+    print("Not leap year")
+elif(year%400 ==0):
+    print("Leap year")
+else:
+    print("Not leap yar")
+
+
+
+
+    
